718cc7f73ea2eb9ab35113c153f80697417740a0
XSR1 <47316244+XSR1@users.noreply.github.com>
2023-12-20 17:51:02 +0200

    Update trusted.sh

--- network/trusted.sh
+++ network/trusted.sh
@@ -2,7 +2,7 @@
 true
 SCRIPT_NAME="Set trusted domain"
 # shellcheck source=lib.sh
-source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+source <(curl -sL https://raw.githubusercontent.com/XSR1/Nextcloud-VM/master/lib.sh)
 
 # Removed in NC 26.0.0.
 
